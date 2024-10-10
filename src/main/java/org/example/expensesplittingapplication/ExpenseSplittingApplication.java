43199c3ed1f1deafdf12380d4cd81acf43a19a07
Utkarsh <43597586+utkarshs106@users.noreply.github.com>
2024-10-10 18:58:35 +0530

    initial commit

---
+++ src/main/java/org/example/expensesplittingapplication/ExpenseSplittingApplication.java
@@ -0,0 +1,13 @@
+package org.example.expensesplittingapplication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExpenseSplittingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExpenseSplittingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/example/expensesplittingapplication/ExpenseSplittingApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.expensesplittingapplication;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExpenseSplittingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
